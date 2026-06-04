3d2eb1cdfd599b111c59d7e281a0de00a85c6a12
Trisstan1601 <tristan.barsties@ess-elisabeth.de>
2026-06-04 11:01:16 +0200

    Update app.py

--- app.py
+++ app.py
@@ -46,9 +46,7 @@ def drehen(aktueller_einsatz):
     elif w1 == w2 or w2 == w3 or w1 == w3:
         st.session_state.kontostand += 15
         st.info("✨ Gut gemacht! 2 gleiche Symbole! Du gewinnst 15 Punkte!")
-
-    st.rerun()
-        
+    st.rerun() 
     else:
         st.warning("Leider kein Gewinn. Versuch es noch einmal!")
 
